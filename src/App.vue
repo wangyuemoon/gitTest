724de836c1a9116545fa3ace393881952d9e68a1
wangyuemoon <15021195331@163.com>
2019-04-22 11:11:59 +0800

    修改app.vue

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
         <span>{{good.text}}</span>
         <span>{{good.text}}</span>
         <span>￥{{good.price}}</span>
+        <span>6777fvfv</span>
         <p>358282743</p>
         <p>
           <input type="text" v-model="lala">
